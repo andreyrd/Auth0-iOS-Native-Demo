29a47056f59207923c96947ba8967afcba4c9070
Andrey Radchishin <andreyrd1@gmail.com>
2018-02-16 12:20:10 -0800

    Initial Commit

---
+++ Auth0-iOS-Native-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Auth0-iOS-Native-Demo
+//
+//  Created by Andrey Radchishin on 2/16/18.
+//  Copyright © 2018 Andrey Radchishin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
